4e01a5f2065d98a337f538aa3794b850fa25cdf3
unknown <reaganpang@yahoo.com>
2020-05-02 15:31:18 +0800

    added side-nav module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideNavModule } from './modules/side-nav/side-nav.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SideNavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
